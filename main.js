9ad0dd32363260a14313d1ca0320eda50dc04389
Tanishq Iyer <tanishqiyer88@gmail.com>
2024-01-20 22:46:45 -0500

    a

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ function initOptions(options, defaults) {
     if (isUdf(options)) {
         options = {};
     } else if (!isObject(options)) {
-        // throw new Error("Invalid Argument.");
-        options = {};
+        throw new Error("Invalid Argument.");
     }
 
     return Object.assign({}, defaults, options);
 }
+
+const ALL_ITEMS = {};
 
 const ITEM_BOX_DEFAULTS = {};
 
@@ -25,13 +26,17 @@ class ItemBox {
         this._root = root;
         this._options = options = initOptions(options, ITEM_BOX_DEFAULTS);
 
+        if (!(options.topic in ALL_ITEMS)) {
+            ALL_ITEMS[options.topic] = [];
+        }
+        ALL_ITEMS[options.topic].push(this);
+
         this._createAndAttachElements();
     }
 
     _createAndAttachElements() {
         this._item = document.createElement("div");
         this._item.classList.add("item");
-        this._root.append(this._item);
 
         this._imageLink = document.createElement("a");
         if (this._options.itemLink) {
@@ -62,16 +67,101 @@ class ItemBox {
         this._title.classList.add("item__title");
         this._item.append(this._title);
     }
+
+    attach() {
+        this._root.append(this._item);
+    }
+
+    detach() {
+        this._item.remove();
+    }
+}
+
+let CUR_TOPIC = null;
+
+function setTopic(newTopic) {
+    if (CUR_TOPIC !== null) {
+        ALL_ITEMS[CUR_TOPIC].forEach((item) => {
+            item.detach();
+        });
+    }
+
+    ALL_ITEMS[newTopic].forEach((item) => {
+        item.attach();
+    });
+
+    CUR_TOPIC = newTopic;
 }
 
 function main() {
     const root = document.getElementById("root");
 
+    const webDevButton = document.getElementById("general-button");
+    const gameDevButton = document.getElementById("game-dev-button");
+
+    webDevButton.addEventListener("click", function () {
+        setTopic("general");
+    });
+
+    gameDevButton.addEventListener("click", function () {
+        setTopic("game-dev");
+    });
+
     const feed = document.createElement("div");
     feed.classList.add("feed");
     root.appendChild(feed);
 
+
+
+
+    const bookmarkManager = new ItemBox(feed, {
+        topic: "general",
+        title: "Bookmark Manager Extension",
+        imageUrl: "./images/bookmark.svg",
+        itemLink: "https://chromewebstore.google.com/detail/bookmark-manager/bpbkeoboacafeolmjaidheedfeapfike",
+        githubUrl: "https://github.com/tniyer2/bookmark-manager-nodejs"
+    });
+
+    const imageEditor = new ItemBox(feed, {
+        topic: "general",
+        title: "Image Editor WebApp",
+        imageUrl: "./images/image-editor.jpg",
+        itemLink: "./image-editor",
+        githubUrl: "https://github.com/tniyer2/image-editor-nodejs"
+    });
+
+    const framework = new ItemBox(feed, {
+        topic: "general",
+        title: "JS Framework",
+        itemLink: "https://github.com/tniyer2/framework",
+        githubUrl: "https://github.com/tniyer2/framework"
+    });
+
+    const jsClass = new ItemBox(feed, {
+        topic: "general",
+        title: "JS Class System",
+        itemLink: "https://github.com/tniyer2/jsclass",
+        githubUrl: "https://github.com/tniyer2/jsclass"
+    });
+
+    const steamReviewsDataWrangling = new ItemBox(feed, {
+        topic: "general",
+        title: "Steam Reviews Data Wrangling",
+        itemLink: "https://github.com/tniyer2/Steam_Reviews_Data_Wrangling",
+        githubUrl: "https://github.com/tniyer2/Steam_Reviews_Data_Wrangling"
+    });
+
+    const chessImageDetection = new ItemBox(feed, {
+        topic: "general",
+        title: "Chess Image Detection",
+        itemLink: "https://github.com/tniyer2/chess_image_detection",
+        githubUrl: "https://github.com/tniyer2/chess_image_detection"
+    });
+
+
+
     const rubiksCube = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Rubik's Cube Simulation",
         imageUrl: "./images/rubiks-cube.png",
         itemLink: "./rubiks-cube-graphics",
@@ -79,6 +169,7 @@ function main() {
     });
 
     const flightSim = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Flight Simulator",
         imageUrl: "./images/flight-sim.png",
         itemLink: "./flight-simulator",
@@ -86,6 +177,7 @@ function main() {
     });
 
     const hammerGame = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Hammer Game",
         imageUrl: "./images/hammer-game.png",
         itemLink: "./hammer-game",
@@ -93,53 +185,51 @@ function main() {
     });
 
     const snakeGame = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Snake Game",
         imageUrl: "./images/snake-game.jpeg",
         itemLink: "./snake",
         githubUrl: "https://github.com/tniyer2/snake"
     });
 
-    const bookmarkManager = new ItemBox(feed, {
-        title: "Bookmark Manager Extension",
-        imageUrl: "./images/bookmark.svg",
-        githubUrl: "https://github.com/tniyer2/bookmark-manager-nodejs"
-    });
-
-    const imageEditor = new ItemBox(feed, {
-        title: "Image Editor WebApp",
-        imageUrl: "./images/image-editor.jpg",
-        itemLink: "./image-editor",
-        githubUrl: "https://github.com/tniyer2/image-editor-nodejs"
-    });
-
     const delaunayVoronoi = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Delaunay Triangulation",
         imageUrl: "./images/delaunay-voronoi.jpeg",
         githubUrl: "https://github.com/tniyer2/delaunay-voronoi"
     });
 
     const tEngine = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Basic C++ Game Engine",
         imageUrl: "./images/cplusplus.png",
         githubUrl: "https://github.com/tniyer2/t-engine"
     });
 
     const voxelsUnity = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Mesh to Voxels using Surface Net",
         imageUrl: "./images/voxels.png",
         githubUrl: "https://github.com/tniyer2/voxels"
     });
 
     const rustGraphics = new ItemBox(feed, {
+        topic: "game-dev",
         title: "Vulkan in Rust",
         imageUrl: "./images/rust.png",
         githubUrl: "https://github.com/tniyer2/rust_engine"
     });
 
     const nimGraphics = new ItemBox(feed, {
+        topic: "game-dev",
         title: "OpenGL in Nim",
         imageUrl: "./images/nim.png",
         githubUrl: "https://github.com/tniyer2/nim-graphics"
     });
+
+
+
+    setTopic("general");
 }
+
 main();
